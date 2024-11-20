a8b0c1c4ffd70b003ded198c32b99b5d0283372a
Om Sharma <133844586+OmSharma42306@users.noreply.github.com>
2024-11-20 16:32:58 +0530

    Update page.tsx
    Added About

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Om Sharma, a Full Stack Developer passionate about building robust, scalable, and efficient web applications. I specialize in the MERN stack—MongoDB, Express.js, React, and Node.js—while leveraging additional expertise in PostgreSQL, Prisma ORM, and Docker. With skills in Python, WebRTC, and socket programming, I thrive on creating seamless real-time communication experiences. My experience extends to cloud platforms like AWS and Kubernetes, as well as modern serverless architecture.
+
+Proficient in TypeScript and JavaScript, I combine my knowledge of DSA, DBMS, OS, and Computer Networks to design and implement optimized solutions. Whether tackling complex backend systems or crafting intuitive frontends, I am driven by a commitment to clean, maintainable, and impactful code.`}
       </p>
       <div className="my-8">
         <BlogPosts />
